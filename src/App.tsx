14c2dfc17f1bc8ce3516a9a47fbbfc91dbbb2b04
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-05-06 23:02:21 +0200

    import <Header />

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.scss";
 import { Navigation } from "./component/Navigation/Navigation";
+import { Header } from "./component/Header/Header";
 
 function App() {
   return (
     <>
-      <Navigation />
+      <Header />
     </>
   );
 }
