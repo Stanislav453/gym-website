95b1f2573fb5afda3d7dcb7973c22654fac16450
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-05-06 23:08:24 +0200

    change code consistency

--- src/component/Navigation/Navigation.tsx
+++ src/component/Navigation/Navigation.tsx
@@ -4,8 +4,10 @@ import { nav_title, nav_icon, nav_logo } from "./Data";
 export const Navigation = () => {
   return (
     <nav className="navigation">
-      <div className="navigation-logo">
-        <img src={nav_logo} alt="logo" width="75px" />
+      <div className="navigation-logo-box">
+        <a href="#">
+          <img className="navigation-logo-box-img" src={nav_logo} alt="logo" />
+        </a>
       </div>
       <ul className="navigation-ul">
         {nav_title.map((item, key) => {
@@ -19,12 +21,12 @@ export const Navigation = () => {
           );
         })}
       </ul>
-      <ul className="navigation-ul_icon">
+      <ul className="navigation-ul-icon">
         {nav_icon.map((item, key) => {
           const { icon, link } = item;
           return (
             <li key={key}>
-              <a href={link} target="_blank">
+              <a className="navigation-ul-icon-a" href={link} target="_blank">
                 {icon}
               </a>
             </li>
