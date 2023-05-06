ec4b01a302820dda9bf3001a53536f14c2f98a37
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-05-06 23:08:41 +0200

    add Data for imageSlider

---
+++ src/component/SlideShow/Data.ts
@@ -0,0 +1,11 @@
+import img1 from "./img/img_1.webp";
+import img2 from "./img/img_2.webp";
+import img3 from "./img/img_3.webp";
+import img4 from "./img/img_4.webp";
+
+export const Data = [
+  { img: img1, alt: "foto_1" },
+  { img: img2, alt: "foto_2" },
+  { img: img3, alt: "foto_3" },
+  { img: img4, alt: "foto_4" },
+];
